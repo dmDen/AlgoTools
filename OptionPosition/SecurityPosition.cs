f3e2146e07e1daddabbe31d4148aa4deef7542ca
Den <DenPkr@mail.ru>
2016-02-10 16:17:47 +0500

    Start OptionPosition Project

---
+++ OptionPosition/SecurityPosition.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionPosition
+{
+	class SecurityPosition
+	{
+		/// <summary>
+		/// Активна ли позиция
+		/// </summary>
+		public bool IsActive
+		{
+			get; set;
+		}
+
+		/// <summary>
+		/// Тикер опциона
+		/// </summary>
+		public string OptionCode
+		{
+			get; set;
+		}
+
+		/// <summary>
+		/// Объем позиции
+		/// </summary>
+		public decimal Volume
+		{
+			get; set;
+		}
+
+
+	}
+}
